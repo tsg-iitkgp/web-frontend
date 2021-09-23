a1101a51174116115c02483b40b303fe66818a81
mySpaceHere123 <ujjwal.raj.5798@gmail.com>
2021-09-23 20:57:21 +0530

    Society Cards

---
+++ src/pages/SocAndCell.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Section from "../components/Section";
+import Styles from "../styles/components/SocAndCell.module.css";
+
+export default function SocAndCell() {
+  return (
+    <div className={Styles.socs}>
+      <div className={Styles.container}>
+        <div className={Styles.card}>
+          <div className={`${Styles.face} ${Styles.face1} ${Styles.sc}`}>
+            <div className={Styles.content}>
+              <h3>Social and Cultural</h3>
+            </div>
+          </div>
+          <div className={`${Styles.face} ${Styles.face2} `}>
+            <div className={Styles.content}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                cum cumque minus iste veritatis provident at.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className={Styles.card}>
+          <div className={`${Styles.face} ${Styles.face1} ${Styles.te}`}>
+            <div className={Styles.content}>
+              <h3>Tech and Educational</h3>
+            </div>
+          </div>
+          <div className={`${Styles.face} ${Styles.face2}`}>
+            <div className={Styles.content}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                cum cumque minus iste veritatis provident at.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className={Styles.card}>
+          <div className={`${Styles.face} ${Styles.face1} ${Styles.rg}`}>
+            <div className={Styles.content}>
+              <h3>Research Groups</h3>
+            </div>
+          </div>
+          <div className={`${Styles.face} ${Styles.face2}`}>
+            <div className={Styles.content}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                cum cumque minus iste veritatis provident at.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
