fd2ecfc53da2cf61a9e3274695684511781bebb5
Nirmal Kumawat <nirmal.kumawat@kgpian.iitkgp.ac.in>
2021-10-05 21:15:40 +0530

    Removed autoplay slider

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -7,18 +7,20 @@ function Contacts() {
     document.title = 'Contacts | TSG';
     return (
         <Layout>
-            <h1 className='pageHeading'>
-                Contact Us
-            </h1>
-            <div className={Styles.contactsContainer}>
-                <ContactCard
-                    imgSrc={'/data/images/contacts/19AE10017.png'}
-                    name="Praneeth"
-                    designation="Technology Coordinator"
-                    email="praneeth"
-                    facebook=""
-                    whatsapp=""
-                />
+            <div className=''>
+                <h1 className='pageHeading'>
+                    Contact Us
+                </h1>
+                <div className={Styles.contactsContainer}>
+                    <ContactCard
+                        imgSrc={'/data/images/contacts/19AE10017.png'}
+                        name="Praneeth"
+                        designation="Technology Coordinator"
+                        email="praneeth"
+                        facebook=""
+                        whatsapp=""
+                    />
+                </div>
             </div>
         </Layout>
     )
