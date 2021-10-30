62be5e7e5de5669066e547bcfc9b0f51f967cb66
Nirmal Kumawat <nirmal.kumawat@kgpian.iitkgp.ac.in>
2021-10-30 21:32:52 +0530

    Changed Events page titles

--- src/components/Events/EventCard.js
+++ src/components/Events/EventCard.js
@@ -5,21 +5,45 @@ export default function EventCard({
   title,
   date,
   description,
+  index,
   bodyContent,
   eventCategory: category,
   imgName,
 }) {
   const imgSrc = `/media/images/events/${category}/${imgName}`;
+  let blogCardClass;
+  if (index % 2 !== 0) {
+    blogCardClass = `${Styles.blogCard} ${Styles.alt}`;
+  } else {
+    blogCardClass = `${Styles.blogCard}`;
+  }
   return (
-    <div className={Styles.cardContainer}>
-      <div className={Styles.imgContainer}>
-        <img src={imgSrc} alt={title} />
+    <div className={blogCardClass}>
+      <div className={Styles.meta}>
+        <div
+          className={Styles.photo}
+          style={{
+            backgroundImage:
+              "url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)",
+          }}
+        ></div>
+        <ul className={Styles.details}>
+          <li className={Styles.author}>
+            <a href="#">John Doe</a>
+          </li>
+          <li className={Styles.author}>
+            <a href="#">+91 11111 11111</a>
+          </li>
+        </ul>
       </div>
-      <div className={Styles.content}>
-        <div className={Styles.title}>{title}</div>
-        <div className={Styles.date}>{date}</div>
-        <div className={Styles.description}>{description}</div>
-        <button>Learn More</button>
+      <div className={Styles.description}>
+        <h1>{title}</h1>
+        <h2>{date}</h2>
+        <h2>Opening a door to the future</h2>
+        <p> {description}</p>
+        <p className={Styles.readMore}>
+          <a href="#">Read More</a>
+        </p>
       </div>
     </div>
   );
