16194410e5a3236a42d50a5f45914d9fc1fafa26
Nitish <nitish.bpns@gmail.com>
2021-11-01 15:54:14 +0530

    Revert "added events, updated footer width"
    This reverts commit 86869155ecccb88d3c3261f62050e9ab8ba4c15a.

--- src/App.js
+++ src/App.js
@@ -30,17 +30,6 @@ function App() {
         <Route exact path="/booking" component={Booking} />
         <Route exact path="/contacts" component={Contacts} />
         <Route exact path="/login" component={Login} />
-<<<<<<< Updated upstream
-=======
-        <Route exact path="/results/gc" component={Results} />
-        <Route exact path="/results/inter_iit" component={Results} />
-        <Route exact path="/contacts/secretaries" component={Contacts} />
-        <Route exact path="/contacts/currentSenate" component={Contacts} />
-        <Route exact path="/contacts/pastBearers" component={Contacts} />
-        <Route exact path="/societies" component={Societies} />
-        <Route exact path="/events" component={Events} />
-
->>>>>>> Stashed changes
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -4,7 +4,6 @@ import Layout from '../components/Layout';
 import Styles from '../styles/contacts.module.css';
 
 function Contacts() {
-<<<<<<< Updated upstream
     document.title = 'Contacts | TSG';
     return (
         <Layout>
@@ -23,51 +22,6 @@ function Contacts() {
             </div>
         </Layout>
     )
-=======
-  document.title = "Contacts | TSG";
-  // state variable for rendering content on the basis of sidebar click
-  const [currentSenate, setCurrentSenate] = useState(true);
-  const [secretaries, setSecretaries] = useState(false);
-  const [pastBearers, setPastBearers] = useState(false);
-
-  const location = useLocation();
-
-  useEffect(() => {
-    //changing state variable values on the basis of path
-    if (location.pathname === "/contacts") {
-      setCurrentSenate(true);
-      setPastBearers(false);
-      setSecretaries(false);
-    } else if (location.pathname === "/contacts/currentSenate") {
-      setCurrentSenate(true);
-      setPastBearers(false);
-      setSecretaries(false);
-    } else if (location.pathname === "/contacts/secretaries") {
-      setCurrentSenate(false);
-      setPastBearers(false);
-      setSecretaries(true);
-    } else if (location.pathname === "/contacts/PastBearers") {
-      setCurrentSenate(false);
-      setPastBearers(true);
-      setSecretaries(false);
-    }
-  }, [location.pathname]);
-
-  return (
-    <Layout>
-      <div className={Styles.maincontainer}>
-        <Sidebar itemsList={contactsSidebarList} />
-
-        <div className={Styles.content}>
-          {/* Contacts of the page on basis of route */}
-          {currentSenate && <CurrentSenate />}
-          {secretaries && <Secretaries />}
-          {pastBearers && <PastBearers />}
-        </div>
-      </div>
-    </Layout>
-  );
->>>>>>> Stashed changes
 }
 
 export default Contacts;
