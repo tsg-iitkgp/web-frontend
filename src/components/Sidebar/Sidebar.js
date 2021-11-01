9a502f2e4937319436f5d8a4a3335576a56b21a4
Nirmal Kumawat <nirmal.kumawat@kgpian.iitkgp.ac.in>
2021-11-01 16:54:44 +0530

    Improved sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import Styles from "../../styles/components/Sidebar/sidebar.module.css";
+import Styles from "./sidebar.module.css";
 export default function Sidebar({ itemsList }) {
+  const [active, setActive] = useState(false);
+  function onMouseEnter() {
+    setActive(true);
+  }
+  function onMouseLeave() {
+    setActive(false);
+  }
   return (
-    <div className={Styles.sidebar}>
-      <ul className={Styles.itemlist}>
-        {itemsList.map((item, index) => {
-          return (
-            <li key={index}>
-              <NavLink
-                to={item.route}
-                className={Styles.sidebarItem}
-                activeClassName={Styles.active}
-              >
-                {item.title}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul
+      className={
+        active ? `${Styles.sidebar} ${Styles.active}` : `${Styles.sidebar}`
+      }
+      onMouseEnter={() => onMouseEnter()}
+      onMouseLeave={() => onMouseLeave()}
+    >
+      {itemsList.map((item, index) => {
+        return (
+          <li key={index}>
+            <NavLink
+              to={item.route}
+              className={Styles.sidebarItem}
+              activeClassName={Styles.active}
+            >
+              {item.title}
+            </NavLink>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
