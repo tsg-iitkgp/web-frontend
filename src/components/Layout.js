8cb3850b850cd2ad64772db38610f0e078e5db4f
Nirmal Kumawat <nirmal.kumawat@kgpian.iitkgp.ac.in>
2021-11-02 18:31:13 +0530

    Updated Contact us page and contacts page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,6 @@ import { FaFacebook, FaGithub, FaTwitter, FaYoutube } from "react-icons/fa";
 import FooterStyles from "../styles/components/footer.module.css";
 import logo from "../images/IIT_Kharagpur_Logo.png";
 import Navbar from "./Navbar/Navbar";
-import { ArrowLeft } from "react-bootstrap-icons";
 import "../styles/components/footer.module.css";
 
 export default function Layout({ children }) {
@@ -17,7 +16,11 @@ export default function Layout({ children }) {
           <div className={FooterStyles.footerCol}>
             <center>
               <div className={FooterStyles.columnOne}>
-                <img src={logo} alt="IITKGP Logo" className={FooterStyles.imgLogo}/>
+                <img
+                  src={logo}
+                  alt="IITKGP Logo"
+                  className={FooterStyles.imgLogo}
+                />
                 <div className={FooterStyles.address}>
                   <div>
                     <br />
@@ -49,72 +52,118 @@ export default function Layout({ children }) {
           </div>
           <div className={FooterStyles.footerCol}>
             <div className={FooterStyles.columnTwo}>
-              <a className={FooterStyles.quickLinkHeader}>
+              <a className={FooterStyles.quickLinkHeader} href=" ">
                 Quick Links
               </a>
               <br />
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="http://www.counsellingcentre.iitkgp.ac.in/" target="_blank">
-              Counselling Centre
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="http://www.counsellingcentre.iitkgp.ac.in/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Counselling Centre
               </a>
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="http://www.iitkgp.ac.in/" target="_blank">
-              IITKGP Website
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="http://www.iitkgp.ac.in/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                IITKGP Website
               </a>
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="https://wiki.metakgp.org/w/Yellow_pages/" target="_blank">
-              Yellow Pages
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="https://wiki.metakgp.org/w/Yellow_pages/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Yellow Pages
               </a>
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="http://www.hmc.iitkgp.ac.in/web/" target="_blank">
-              Hall Management Centre
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="http://www.hmc.iitkgp.ac.in/web/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Hall Management Centre
               </a>
             </div>
           </div>
           <div className={FooterStyles.footerCol}>
             <div className={FooterStyles.columnThree}>
-              <a className={FooterStyles.quickLinkHeader}>
+              <a className={FooterStyles.quickLinkHeader} href=" ">
                 Quick Links
               </a>
               <br />
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken=F90F79C925A0FE0DC48AE7CBFDE3D20A.worker1&requestedUrl=https://erp.iitkgp.ac.in/IIT_ERP3/" target="_blank">
-              ERP
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken=F90F79C925A0FE0DC48AE7CBFDE3D20A.worker1&requestedUrl=https://erp.iitkgp.ac.in/IIT_ERP3/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                ERP
               </a>
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="https://iitkgpmail.iitkgp.ac.in/" target="_blank">
-              Zimbra webmail
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="https://iitkgpmail.iitkgp.ac.in/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Zimbra webmail
               </a>
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="https://yourdost.com/" target="_blank">
-              Your Dost
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="https://yourdost.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Your Dost
               </a>
               <br />
               <br />
-              <a className={FooterStyles.quickLinkOne} href="https://docs.google.com/forms/d/e/1FAIpQLSft9FXZYvLiNt3oy0K3Iu4d6HoE830RhNWFHMXx1R4IjlhHKA/viewform" target="_blank">
-              Grievance Form
+              <a
+                className={FooterStyles.quickLinkOne}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSft9FXZYvLiNt3oy0K3Iu4d6HoE830RhNWFHMXx1R4IjlhHKA/viewform"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Grievance Form
               </a>
             </div>
           </div>
           <div className={FooterStyles.footerCol}>
             <div className={FooterStyles.columnFour}>
-            <div md={7} style={{marginBottom:"3%"}}>
-                    <h2 style={{marginTop:"20px"}} className={FooterStyles.footer_heading1}>We're here</h2>
-                    <h2 className={FooterStyles.footer_heading2}>Let's talk</h2>
-                    <a href="/ContactUs" target="_blank"><button style={{ marginLeft: "0%"}} className="btn_interiit">
-                        Get in Touch </button></a>
-                </div>
+              <div md={7} style={{ marginBottom: "3%" }}>
+                <h2
+                  style={{ marginTop: "20px" }}
+                  className={FooterStyles.footer_heading1}
+                >
+                  We're here
+                </h2>
+                <h2 className={FooterStyles.footer_heading2}>Let's talk</h2>
+                <a href="/ContactUs">
+                  <button style={{ marginLeft: "0%" }} className="btn_interiit">
+                    Get in Touch{" "}
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-         
-        
         </div>
       </footer>
     </div>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -4,97 +4,87 @@ import UpcomingEvents from "../components/Events/UpcomingEvents";
 import OngoingEvents from "../components/Events/UpcomingEvents";
 import EventCard from "../components/Events/EventCard";
 import Layout from "../components/Layout";
-import { Carousel } from 'react-carousel-minimal';
+import { Carousel } from "react-carousel-minimal";
 
 export default function Events() {
-
   const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Cyclothon.jpg",
-      caption: "cyclothon"
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/c/c2/Cyclothon.jpg",
+      caption: "cyclothon",
     },
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/CaseStudy.jpg",
-      caption: "case study"
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/a/ab/CaseStudy.jpg",
+      caption: "case study",
     },
     {
       image: "https://upload.wikimedia.org/wikipedia/commons/8/84/Iitkgp.jpg",
-      caption: "iit kgp"
+      caption: "iit kgp",
     },
-   
   ];
 
   const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  }
+    fontSize: "2em",
+    fontWeight: "bold",
+  };
   const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
-
+    fontSize: "20px",
+    fontWeight: "bold",
+  };
 
   document.title = "Events | TSG";
   return (
     <Layout>
+      <div className="App">
+        <div className={Styles.mainContainer}>
+          {/* Recents Events */}
 
+          <div className={Styles.categoryContainer}>
+            <h2 className={Styles.categoryHeading1}>Recent Events</h2>
+            <div className={Styles.recentEventsSlider}></div>
+          </div>
 
-<div className="App">
-
-<div className={Styles.mainContainer}>
-
-       {/* Recents Events */}
-
-       <div className={Styles.categoryContainer}>
-          <h2 className={Styles.categoryHeading1}>Recent Events</h2>
-          <div className={Styles.recentEventsSlider}></div>
-        </div>
-
-
-
-      <div style={{ textAlign: "center" }}>
-
-        <div  style={{
-          padding: "0 20px"
-        }}>
-          <Carousel 
-            data={data}
-            time={2000}
-            width="100%"
-            height="600px"
-            captionStyle={captionStyle}
-            radius="10px"
-            slideNumber={true}
-            slideNumberStyle={slideNumberStyle}
-            captionPosition="bottom"
-            automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="darkgrey"
-            slideImageFit="cover"
-            thumbnails={true}
-            thumbnailWidth="100px"
-            style={{
-              textAlign: "center",
-              width: "90%",
-              maxHeight: "600px",
-              margin: "40px auto",
-              height:"600px",
-              marginBottom:"10%"
-             
-            }}
-            className={Styles.recentCaro}
-          />
+          <div style={{ textAlign: "center" }}>
+            <div
+              style={{
+                padding: "0 20px",
+              }}
+            >
+              <Carousel
+                data={data}
+                time={2000}
+                width="100%"
+                height="600px"
+                captionStyle={captionStyle}
+                radius="10px"
+                slideNumber={true}
+                slideNumberStyle={slideNumberStyle}
+                captionPosition="bottom"
+                automatic={true}
+                dots={true}
+                pauseIconColor="white"
+                pauseIconSize="40px"
+                slideBackgroundColor="darkgrey"
+                slideImageFit="cover"
+                thumbnails={true}
+                thumbnailWidth="100px"
+                style={{
+                  textAlign: "center",
+                  width: "90%",
+                  maxHeight: "600px",
+                  margin: "40px auto",
+                  height: "600px",
+                  marginBottom: "10%",
+                }}
+                className={Styles.recentCaro}
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
 
       <div className={Styles.mainContainer}>
-   
-
-
         {/* Upcoming Events Container */}
 
         <div className={Styles.categoryContainer}>
