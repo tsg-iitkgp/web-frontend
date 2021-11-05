f8b4324499237d378a40a1e08ecf6ac3b678f15b
Nitish <nitish.bpns@gmail.com>
2021-11-05 13:56:52 +0530

    updated previous senate
    updated previous year senate year wise

--- src/pages/Contacts/PastBearers.js
+++ src/pages/Contacts/PastBearers.js
@@ -1,83 +1,140 @@
 import React from "react";
-import senateData from "../../data/senateData";
 import Styles from "../../styles/pages/past.module.css";
+import senateData from "./../senateData.json";
+import { useState } from "react";
+import ContactCard from "../../components/ContactCard";
+
+
+const years = ["2020-21", "2019-20", "2018-19", "2017-18", "2016-17", "2015-16"];
+
+
 export default function PastBearers() {
+
+  const [currentTab, setCurrentTab] = useState("senate");
+  const [currentYear, setCurrentYear] = useState("2020-21");
+
+  const handleTabChange = (s) => {
+    setCurrentTab(s);
+  };
+
   const president = senateData.President;
   return (
-    <div className={Styles.sbody}>
+    
+    <div>
       <center>
-        <h1>Presidents</h1>
 
-        {president.map((obj) => {
-          return (
-            <>
-              <tr className={Styles.row1}>
-                <td className={Styles.col1p}>{`${obj.year}`}</td>
-                <td className={Styles.col2p}> {`${obj.name}`}</td>
-              </tr>
-            </>
-          );
-        })}
+    
+
+      <section className="awards content">
+ 
+
+        <div className="tabs">
+    
+     
+         
+        </div>
+        <div className="select GC_dropdown">
+          <select
+            value={currentYear}
+            onChange={(e) => setCurrentYear(e.target.value)}
+          >
+            {years.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {(senateData[currentYear][currentTab]["profs"] !== undefined ||
+          currentTab === "specialRecog") && (
+          <>
+            <h2
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                padding: "3%",
+                alignItems: "center",
+                color: "#fff",
+              }}
+            >
+              {" "}
+              Previous   Year  Senate{" "}
+            </h2>
+            <div className="cards">
+              {currentTab !== "specialRecog"
+                ? senateData[currentYear][currentTab]["profs"]?.map(
+                    (winner) => (
+                      <ContactCard
+                        name={winner.Name}
+                        imgSrc={""}
+                        designation={winner.Post}
+                      />
+                    )
+                  )
+                : senateData[currentYear][currentTab].map((winner) => (
+                    <ContactCard
+                      name={winner.Name}
+                      imgSrc={""}
+                      designation={winner.Post}
+                    />
+                  ))}
+            </div>
+          </>
+        )}
+
+        {currentTab !== "specialRecog" && (
+          <>
+            <h2
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                padding: "3%",
+                alignItems: "center",
+                color: "#fff",
+              }}
+            >
+              {" "}
+              {" "}
+            </h2>
+            <div className="table-container">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Post</th>
+                    <th>Name</th>
+                 
+                  </tr>
+                </thead>
+                <tbody>
+                  {senateData[currentYear][currentTab]["honours"].map(
+                    (winner) => (
+                      <tr>
+                        <td>{winner.Post}</td>
+                        <td>{winner.Name} </td>
+          
+                       
+                      </tr>
+                    )
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
+      </section>
+
+
+
+      
         <br />
         <br />
-        {/* <h1>
-        Honrary Treasurer</h1>
-
-   
-     {honTreasurer.map((obj)=>{
-
-
-     return<>
      
-     <tr className={Styles.row1}>
-            <td className={Styles.col1}>{`${obj.year}`}</td>
-            <td className={Styles.col2}> {`${obj.name}`}</td>
-
-     </tr>
-     
-     </>
- })
-} */}
         <br />
         <br />
-        {/* <h1>
-        Vice President</h1>
-
-   
-     {vicePresident.map((obj)=>{
-
-
-     return<>
-     
-     <tr className={Styles.row1}>
-            <td className={Styles.col1}>{`${obj.year}`}</td>
-            <td className={Styles.col2}> {`${obj.name}`}</td>
-
-     </tr>
-     
-     </>
- })
-}
-<br/>
-<br/>
-<h1>
-        Vice President</h1>
-
-   
-     {vicePresident.map((obj)=>{
-
-
-     return<>
-     
-     <tr className={Styles.row1}>
-            <td className={Styles.col1}>{`${obj.year}`}</td>
-            <td className={Styles.col2}> {`${obj.name}`}</td>
-
-     </tr>
-     
-     </>
- })
-} */}
+      
       </center>
     </div>
   );
