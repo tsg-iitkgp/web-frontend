034c92bb8b76c01cf529c5df5479218647b9561c
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-04-03 16:46:44 +0530

    Notif fixes

--- src/components/Navbar/Notification.js
+++ src/components/Navbar/Notification.js
@@ -5,7 +5,7 @@ export default function Notification({ event, handleClick }) {
   return (
     <div className={Styles.notification} onClick={() => handleClick()}>
       <div>
-        <span style={{ color: "rgba(0,0,0,0.6)", fontSize: "1rem" }}>
+        <span style={{ color: "rgba(0,0,0,0.5)", fontSize: "1.1rem", fontWeight: '400' }}>
           {event.title}
           <span
             class="material-icons"
@@ -22,7 +22,7 @@ export default function Notification({ event, handleClick }) {
           </span>
         </span>
       </div>
-      <div style={{ paddingTop: "0.25rem" }}>
+      <div style={{ paddingTop: "0.4rem", fontSize: '0.8rem' }}>
         {event.description.slice(0, 150) + ". . . . "}
       </div>
       <div
