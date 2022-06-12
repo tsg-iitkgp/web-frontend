2feef208eb4b647eb3e2f9c81ec83e8194245e0a
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2022-06-12 11:54:08 +0530

    Made contact form functional and added apiService file

---
+++ src/apiService.js
@@ -0,0 +1,4 @@
+// const apiService = 'http://127.0.0.1:5050';
+const apiService = 'https://gymkhana.iitkgp.ac.in';
+
+export default apiService;
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactForm.css";
 import Layout from "../Layout";
 import Styles from '../../styles/components/form.module.css';
+import host from '../../apiService';
 
 function ContactForm() {
+  const [name, setName] = useState('');
+  const [rollNumber, setRollNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setStatusMessage('');
+    const options = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        rollNumber,
+        email,
+        subject,
+        message
+      })
+    }
+    // sign in and get the token
+    fetch(`${host}/api/main/contact-us/`, options)
+      .then((response) => (response.json()))
+      .then((jsonData) => {
+        if (jsonData.success) {
+          setStatusMessage('Form has been successfully submitted.');
+          setIsLoading(false);
+          setName('');
+          setRollNumber('');
+          setEmail('');
+          setSubject('');
+          setMessage('');
+        } else {
+          console.log('Error: ', jsonData.message);
+          setStatusMessage('Form couldn\'t be submitted due to some technical issue. Please try again later.');
+          setIsLoading(false);
+        }
+
+      }).catch((error) => {
+        console.log('Error: ', error);
+        setStatusMessage('Form couldn\'t be submitted due to some technical issue. Please try again later.');
+        setIsLoading(false);
+      });
+  }
   return (
     <Layout>
       <div className="maindiv_loginpage">
@@ -48,52 +98,65 @@ function ContactForm() {
                   <h1>
                     Reach Out To Us
                   </h1>
+                  {
+                    statusMessage && (
+                      <p>
+                        {statusMessage}
+                      </p>
+                    )
+                  }
                 </div>
                 <div className={Styles.body}>
-                  <form className={Styles.form}>
+                  <form onSubmit={submitForm} className={Styles.form}>
                     <div className={Styles.formRow}>
                       <div>
-                        <input type={'text'} required />
+                        <input name="name" value={name} onChange={(e) => (setName(e.target.value))} type={'text'} required />
                         <label htmlFor="name" className={Styles.labelName}>
                           <span className={Styles.contentName}>Name</span>
                         </label>
                       </div>
                       <div>
-                        <input type={'text'} required />
-                        <label htmlFor="name" className={Styles.labelName}>
+                        <input name="rollNumber" value={rollNumber} onChange={(e) => (setRollNumber(e.target.value))} type={'text'} required />
+                        <label htmlFor="rollNumber" className={Styles.labelName}>
                           <span className={Styles.contentName}>Roll Number</span>
                         </label>
                       </div>
                     </div>
                     <div className={Styles.formRow}>
                       <div>
-                        <input type={'email'} required />
-                        <label htmlFor="name" className={Styles.labelName}>
+                        <input name="email" value={email} onChange={(e) => (setEmail(e.target.value))} type={'email'} required />
+                        <label htmlFor="email" className={Styles.labelName}>
                           <span className={Styles.contentName}>Email</span>
                         </label>
                       </div>
                       <div>
-                        <input type={'text'} required />
-                        <label htmlFor="name" className={Styles.labelName}>
+                        <input name="subject" value={subject} onChange={(e) => (setSubject(e.target.value))} type={'text'} required />
+                        <label htmlFor="subject" className={Styles.labelName}>
                           <span className={Styles.contentName}>Subject</span>
                         </label>
                       </div>
                     </div>
                     <div className={Styles.formRow}>
                       <div>
-                        <input type={'text'} required />
-                        <label htmlFor="name" className={Styles.labelName}>
+                        <input name="message" value={message} onChange={(e) => (setMessage(e.target.value))} type={'text'} required />
+                        <label htmlFor="message" className={Styles.labelName}>
                           <span className={Styles.contentName}>
                             Message
                           </span>
                         </label>
                       </div>
                     </div>
-                    <div className={Styles.formRow}>
-                      <button type={'submit'}>
-                        Submit
-                      </button>
-                    </div>
+                    {
+                      isLoading ? (
+                        <></>
+                      ) : (
+                        <div className={Styles.formRow}>
+                          <button type={'submit'}>
+                            Submit
+                          </button>
+                        </div>
+                      )
+                    }
                   </form>
                 </div>
                 <div className={Styles.footer}>
