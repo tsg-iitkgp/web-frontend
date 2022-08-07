bd68d1cf8ff724a47f20072f39b2a34dd98e1c8b
Parth Gupta <parthgupta2510@gmail.com>
2022-08-07 15:59:17 +0530

    added Login page

--- src/pages/admin/Login.js
+++ src/pages/admin/Login.js
@@ -1,12 +1,87 @@
-import React from 'react'
-import Layout from '../../components/Layout';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./LoginScreen.css";
+import Layout from "../../components/Layout";
 
-function Login() {
-    return (
-        <Layout>
-            
-        </Layout>
-    )
-}
+const LoginScreen = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default Login;
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      history.push("/certificate");
+    }
+  }, [history]);
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/login",
+        { email, password },
+        config
+      );
+
+      localStorage.setItem("authToken", data.token);
+
+      history.push("/certificate");
+    } catch (error) {
+      console.log(error);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <Layout>
+      <div className="login-screen">
+        <form onSubmit={loginHandler} className="login-screen__form">
+          <h3 className="login-screen__title">Login</h3>
+          {error && <span className="error-message">{error}</span>}
+          <div className="form-group">
+            <input
+              type="email"
+              required
+              id="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              tabIndex={1}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              required
+              id="password"
+              autoComplete="true"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              tabIndex={2}
+            />
+          </div>
+          <div className="form-button">
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </div>
+
+          <span className="login-screen__subtext">
+          </span>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default LoginScreen;
