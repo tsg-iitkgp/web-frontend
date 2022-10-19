68cd14b9913ab5d430524a6d65f8fb459aa6f57a
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2022-10-19 21:21:58 +0530

    add skeleton to the events

---
+++ src/components/Skeleton/SkeletonElement.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Skeleton.css";
+
+const SkeletonElement = ({ type }) => {
+    const classes = `skeleton ${ type }`;
+
+    return (
+        <div className={classes}>
+
+        </div>
+    )
+}
+
+export default SkeletonElement;
