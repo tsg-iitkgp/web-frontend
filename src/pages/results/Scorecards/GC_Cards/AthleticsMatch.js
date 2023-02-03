f1266de68259927e778578fa4aa8af0819b0e050
Krishna Kolte <krishnadk8203@gmail.com>
2023-02-03 18:51:17 +0530

    added GC scoreboards

---
+++ src/pages/results/Scorecards/GC_Cards/AthleticsMatch.js
@@ -0,0 +1,203 @@
+import "../Scorecard.css";
+import * as Icon from "react-feather";
+import Styles from "../../../../styles/components/GC_Live/sports.module.css";
+import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function AthleticsMatch(props) {
+
+    var temp = null;
+    const [data, setData] = useState(null);
+    const [style, setStyle] = useState('fa fa-refresh fa-2x');
+
+    const fetchdata = (url) => {
+        axios.get(url)
+            .then((response) => {
+                let score = response.data;
+                temp = score.data;
+            })
+            .then((responseData) => {
+                setData(temp);
+                setStyle('fa fa-refresh fa-2x');
+                temp = null;
+            })
+    }
+
+    if (props.show === true && data === null) {
+        return (
+            <>
+                <div className="popup" >
+                    <div className="popup-body" data-aos="flip-up" data-aos-duration="1000">
+                        <div className="popup-heading">
+                            <div className='popup-closeButton popup-refresh' style={{ color: "#f1c40f" }} onClick={() => { fetchdata(props.url); setStyle('fa fa-refresh fa-spin fa-2x') }}>
+                                <i className={style} aria-hidden="true"></i>
+                            </div>
+                            <div className="props-title">
+                                {props.heading}
+                            </div>
+                            <div className="popup-closeButton" onClick={() => { props.disable(); setData(null) }} >
+                                <i className="fa fa-times fa-2x" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="popup-content">
+                            <div className="popup-content text">
+                                {props.data.map((val, index) => {
+                                    return (<>
+                                        <div className={Styles.cardContainer}>
+                                            <div className={Styles.timer}>
+                                                {val.timer}
+                                            </div>
+                                            <div className={Styles.header}>
+                                                {val.time}, {val.location}
+                                            </div>
+                                            <div className={Styles.body}>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team1}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        {val.team1_score}
+                                                    </div>
+                                                    <div className={Styles.scorers}>
+                                                        <img src='/images/soccer-ball.png' alt="Err"/>
+                                                        {val.team1_goalscorers}
+                                                    </div>
+                                                </div>
+                                                <div className={Styles.vs}>
+                                                    vs
+                                                </div>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team2}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        {val.team2_score}
+                                                    </div>
+                                                    <div className={Styles.scorers}>
+                                                        <img src='/images/soccer-ball.png' alt="Err"/>
+                                                        {val.team2_goalscorers}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className={Styles.footer}>
+                                                <div>
+                                                    {val.highlight_line}
+                                                </div>
+                                                <div className={Styles.bestPlayer}>
+                                                    <div className={Styles.iconContainer}>
+                                                        <Icon.Award className={Styles.awardIcon} />
+                                                        Best Player of the Match
+                                                    </div>
+                                                    <div className={Styles.bestPlayerName}>
+                                                        {val.best_player}
+                                                    </div>
+                                                </div>
+                                                <div className={Styles.formationBtnContainer}>
+                                                    <a className={Styles.formationBtn} href={val.formation_link} target='_blank' rel="noreferrer">
+                                                        Formation
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>)
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    else if (props.show === true && data !== null) {
+        // console.log(data);
+        return (
+            <>
+                <div className="popup" >
+                    <div className="popup-body" data-aos="flip-up" data-aos-duration="1000">
+                        <div className="popup-heading">
+                            <div className='popup-closeButton popup-refresh' style={{ color: "#f1c40f" }} onClick={() => { fetchdata(props.url); setStyle('fa fa-refresh fa-spin fa-2x') }}>
+                                <i className={style} aria-hidden="true"></i>
+                            </div>
+                            <div className="props-title">
+                                {props.heading}
+                            </div>
+                            <div className="popup-closeButton" onClick={() => { props.disable(); setData(null) }} >
+                                <i className="fa fa-times fa-2x" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="popup-content">
+                            <div className="popup-content text">
+                                {data.map((val, index) => {
+                                    return (<>
+                                        <div className={Styles.cardContainer}>
+                                            <div className={Styles.timer}>
+                                                {val.timer}
+                                            </div>
+                                            <div className={Styles.header}>
+                                                {val.time}, {val.location}
+                                            </div>
+                                            <div className={Styles.body}>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team1}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        {val.team1_score}
+                                                    </div>
+                                                    <div className={Styles.scorers}>
+                                                        <img src='/images/soccer-ball.png' alt="Err"/>
+                                                        {val.team1_goalscorers}
+                                                    </div>
+                                                </div>
+                                                <div className={Styles.vs}>
+                                                    vs
+                                                </div>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team2}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        {val.team2_score}
+                                                    </div>
+                                                    <div className={Styles.scorers}>
+                                                        <img src='/images/soccer-ball.png' alt="Err"/>
+                                                        {val.team2_goalscorers}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className={Styles.footer}>
+                                                <div>
+                                                    {val.highlight_line}
+                                                </div>
+                                                <div className={Styles.bestPlayer}>
+                                                    <div className={Styles.iconContainer}>
+                                                        {/* <Icon.Award size={32} className={Styles.awardIcon} /> */}
+                                                        Best Player of the Match
+                                                    </div>
+                                                    <div className={Styles.bestPlayerName}>
+                                                        {val.best_player}
+                                                    </div>
+                                                </div>
+                                                <div className={Styles.formationBtnContainer}>
+                                                    <a className={Styles.formationBtn} href={val.formation_link} target='_blank' rel="noreferrer">
+                                                        Formation
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>)
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    else {
+        return null;
+    }
+}
---
+++ src/pages/results/Scorecards/GC_Cards/BadmintonMatch.js
@@ -0,0 +1,179 @@
+import "../Scorecard.css";
+import Styles from "../../../../styles/components/GC_Live/badminton.module.css";
+import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function BadmintonMatch(props) {
+
+    var temp = null;
+    const [data, setData] = useState(null);
+    const [style, setStyle] = useState('fa fa-refresh fa-2x');
+
+    const fetchdata = (url) => {
+        axios.get(url)
+            .then((response) => {
+                let score = response.data;
+                // // // console.log(`This is score ${score.data}`);
+                temp = score.data;
+            })
+            .then((responseData) => {
+                // // console.log("fetched again");
+                setData(temp);
+                setStyle('fa fa-refresh fa-2x');
+                // console.log(style);
+                temp = null;
+            })
+    }
+
+    if (props.show === true && data === null) {
+        return (
+            <>
+                <div className="popup" >
+                    <div className="popup-body" data-aos="flip-up" data-aos-duration="1000">
+                        <div className="popup-heading">
+                            <div className='popup-closeButton popup-refresh' style={{ color: "#f1c40f" }} onClick={() => { fetchdata(props.url); setStyle('fa fa-refresh fa-spin fa-2x') }}>
+                                <i className={style} aria-hidden="true"></i>
+                            </div>
+                            <div className="props-title">
+                                {props.heading}
+                            </div>
+                            <div className="popup-closeButton" onClick={() => { props.disable(); setData(null) }} >
+                                <i className="fa fa-times fa-2x" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="popup-content">
+                            <div className="popup-content text">
+                                {props.data.map((val, index) => {
+                                    return (<>
+                                        <div className={Styles.cardContainer}>
+                                            <div className={Styles.header}>
+                                                {val.time}, {val.location}
+                                            </div>
+                                            <div className={Styles.body}>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team1}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Singles - {val.team1_1s_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Doubles - {val.team1_1d_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        2nd Singles - {val.team1_2s_score}
+                                                    </div>
+                                                </div>
+                                                <div className={Styles.vs}>
+                                                    vs
+                                                </div>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team2}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Singles - {val.team2_1s_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Doubles - {val.team2_1d_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        2nd Singles - {val.team2_2s_score}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className={Styles.footer}>
+                                                <div>
+                                                    {val.highlight_line}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>)
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    else if (props.show === true && data !== null) {
+        // console.log(data);
+        return (
+            <>
+                <div className="popup" >
+                    <div className="popup-body" data-aos="flip-up" data-aos-duration="1000">
+                        <div className="popup-heading">
+                            <div className='popup-closeButton popup-refresh' style={{ color: "#f1c40f" }} onClick={() => { fetchdata(props.url); setStyle('fa fa-refresh fa-spin fa-2x') }}>
+                                <i className={style} aria-hidden="true"></i>
+                            </div>
+                            <div className="props-title">
+                                {props.heading}
+                            </div>
+                            <div className="popup-closeButton" onClick={() => { props.disable(); setData(null) }} >
+                                <i className="fa fa-times fa-2x" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="popup-content">
+                            <div className="popup-content text">
+                                {data.map((val, index) => {
+                                    return (<>
+                                        <div className={Styles.cardContainer}>
+                                            <div className={Styles.header}>
+                                                {val.time}, {val.location}
+                                            </div>
+                                            <div className={Styles.body}>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team1}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Singles - {val.team1_1s_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Doubles - {val.team1_1d_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        2nd Singles - {val.team1_2s_score}
+                                                    </div>
+                                                </div>
+                                                <div className={Styles.vs}>
+                                                    vs
+                                                </div>
+                                                <div className={Styles.eachTeamContainer}>
+                                                    <div className={Styles.eachTeam}>
+                                                        {val.team2}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Singles - {val.team2_1s_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        1st Doubles - {val.team2_1d_score}
+                                                    </div>
+                                                    <div className={Styles.teamScore}>
+                                                        2nd Singles - {val.team2_2s_score}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className={Styles.footer}>
+                                                <div>
+                                                    {val.highlight_line}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>)
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    else {
+        return null;
+    }
+}
