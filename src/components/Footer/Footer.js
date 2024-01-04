492c1bff32cbd94a08c37b99d868922ddac9e732
Adarsh <adarshtadiparthi30@gmail.com>
2024-01-04 15:48:55 +0530

    fix:Styling in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,10 @@
 //Footer.js
-
-import React from "react";
+import React , {useState} from "react";
 import Styles from "./footer.module.css";
 import { FaFacebook, FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export default function Footer() {
   const linksList = [
-    {
-      // heading: "Quick Links",
-      links: [
         {
           title: "Counselling Centre",
           href: "http://www.counsellingcentre.iitkgp.ac.in/",
@@ -26,6 +22,7 @@ export default function Footer() {
         },
         {
           title: "HMC",
+          description : "Hall Management Center",
           href: "http://www.hmc.iitkgp.ac.in/web/",
           target: "_blank",
         },
@@ -33,13 +30,7 @@ export default function Footer() {
           title: "Library",
           href: "https://library.iitkgp.ac.in/",
           target: "_blank",  
-        }
-        
-      ],
-    },
-    {
-      // heading: "Quick Links",
-      links: [
+        },
         {
           title: "ERP",
           href: "https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken=F90F79C925A0FE0DC48AE7CBFDE3D20A.worker1&requestedUrl=https://erp.iitkgp.ac.in/IIT_ERP3/",
@@ -59,18 +50,18 @@ export default function Footer() {
         {
           title: "CIC",
           href: "http://www.cic.iitkgp.ac.in/",
-          target: "_blank",  
+          target: "_blank", 
+          description : "Computer & Informatics Center", 
         },
         {
           title:"Apna IIT",
-          description:"On campus network",
+          description:"Accesible on campus network",
           class:"tooltiptext",
           href: "http://apna.iitkgp.ac.in/",
           target:"_blank",
-        }
-      ],
-    },
-  ];
+        }  
+      ]
+
   return (
     <footer>
       <div className={Styles.footerContainer}>
@@ -118,24 +109,21 @@ export default function Footer() {
           </div>
         </div>
         <div className={Styles.linksContainer}>
-          {linksList.map((linkGroup, index) => {
-            return (
-              <div className={Styles.linkGroup} key={index}>
-                {/* <h2>{linkGroup.heading}</h2> */}
-                <ul className={Styles.links}>
-                  {linkGroup.links.map((link, index) => {
-                    return (
-                     <li key={index}>
-                        <a target={link.target} href={link.href} rel="noreferrer">
-                          {link.title}
-                        </a>
-                        {link.description ? <span className={link.class}>{link.description}</span> : <></>}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            );
+          {linksList.map((link , index)=>{
+            return(
+              <p 
+                key={index} 
+              >
+                <a 
+                  target={link.target} 
+                  href={link.href} 
+                  rel="noreferrer" 
+                >
+                  {link.title}
+                </a>
+                {link.description ? <span className={link.class}>{link.description}</span> : <></>} 
+              </p>
+            )
           })}
         </div>
         <div className={Styles.contactSection}>
@@ -146,6 +134,162 @@ export default function Footer() {
           </a>
         </div>
       </div>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
     </footer>
   );
 }
+
+
+// Footer.js
+// import React from "react";
+// import Styles from "./footer.module.css";
+// import { FaFacebook, FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
+
+// export default function Footer() {
+//   const linksList = [
+//     {
+//       // heading: "Quick Links",
+//       links: [
+//         {
+//           title: "Counselling Centre",
+//           href: "http://www.counsellingcentre.iitkgp.ac.in/",
+//           target: "_blank",
+//         },
+//         {
+//           title: "IIT KGP Website",
+//           href: "http://www.iitkgp.ac.in/",
+//           target: "_blank",
+//         },
+//         {
+//           title: "Yellow Pages",
+//           href: "https://wiki.metakgp.org/w/Yellow_pages",
+//           target: "_blank",
+//         },
+//         {
+//           title: "HMC",
+//           href: "http://www.hmc.iitkgp.ac.in/web/",
+//           target: "_blank",
+//         },
+//         {
+//           title: "Library",
+//           href: "https://library.iitkgp.ac.in/",
+//           target: "_blank",  
+//         }
+        
+//       ],
+//     },
+//     {
+//       // heading: "Quick Links",
+//       links: [
+//         {
+//           title: "ERP",
+//           href: "https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken=F90F79C925A0FE0DC48AE7CBFDE3D20A.worker1&requestedUrl=https://erp.iitkgp.ac.in/IIT_ERP3/",
+//           target: "_blank",
+          
+//         },
+//         {
+//           title: "Your Dost",
+//           href: "https://wiki.metakgp.org/w/Yellow_pages/",
+//           target: "_blank",
+//         },
+//         {
+//           title: "Grievance Form",
+//           href: "https://docs.google.com/forms/d/e/1FAIpQLSft9FXZYvLiNt3oy0K3Iu4d6HoE830RhNWFHMXx1R4IjlhHKA/viewform",
+//           target: "_blank",
+//         },
+//         {
+//           title: "CIC",
+//           href: "http://www.cic.iitkgp.ac.in/",
+//           target: "_blank",  
+//         },
+//         {
+//           title:"Apna IIT",
+//           description:"On campus network",
+//           class:"tooltiptext",
+//           href: "http://apna.iitkgp.ac.in/",
+//           target:"_blank",
+//         }
+//       ],
+//     },
+//   ];
+//   return (
+//     <footer>
+//       <div className={Styles.footerContainer}>
+//         <div className={Styles.tsgContainer}>
+//           <div className={Styles.imgContainer}>
+//             <img src="/data/media/images/general/gymkhanaLogo.png" alt="TSG Logo" />
+//           </div>
+//           <div className={Styles.tsgDescription}>
+//             <h2 className={Styles.tsgHeading}>Technology Students' Gymkhana</h2>
+//             <p className={Styles.address}>
+//               Indian Institute of Technology Kharagpur,
+//               <br />
+//               West Bengal - 721302
+//             </p>
+//           </div>
+//           <div className={Styles.iconsContainer}>
+//             <a
+//               target="_blank"
+//               href="https://www.facebook.com/TSG.IITKharagpur"
+//               rel="noreferrer"
+//             >
+//               <FaFacebook />
+//             </a>
+//             <a
+//               target="_blank"
+//               href="https://github.com/tsg-iitkgp"
+//               rel="noreferrer"
+//             >
+//               <FaGithub />
+//             </a>
+//             <a
+//               target="_blank"
+//               href="https://www.youtube.com/channel/UCuTNELMlkNfJxAQ2vGJIe5Q"
+//               rel="noreferrer"
+//             >
+//               <FaYoutube />
+//             </a>
+//             <a
+//               target="_blank"
+//               href="https://www.instagram.com/tsg.iitkharagpur/?hl=en"
+//               rel="noreferrer"
+//             >
+//               <FaInstagram />
+//             </a>
+//           </div>
+//         </div>
+//         <div className={Styles.linksContainer}>
+//           {linksList.map((linkGroup, index) => {
+//             return (
+//               <div className={Styles.linkGroup} key={index}>
+//                 {/* <h2>{linkGroup.heading}</h2> */}
+//                 <ul className={Styles.links}>
+//                   {linkGroup.links.map((link, index) => {
+//                     return (
+//                      <li key={index}>
+//                         <a target={link.target} href={link.href} rel="noreferrer">
+//                           {link.title}
+//                         </a>
+//                         {link.description ? <span className={link.class}>{link.description}</span> : <></>}
+//                       </li>
+//                     );
+//                   })}
+//                 </ul>
+//               </div>
+//             );
+//           })}
+//         </div>
+//         <div className={Styles.contactSection}>
+//           <span>We're here</span>
+//           <span>Let's talk</span>
+//           <a href="/ContactUs">
+//             <button className={Styles.contactButton}>Get in Touch</button>
+//           </a>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// }
