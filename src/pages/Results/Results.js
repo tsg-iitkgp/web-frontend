9e25d14c0902b88236fb3eef81b8412d1380fbf8
proffapt <proffapt@pm.me>
2024-01-05 23:56:50 +0530

    feat: Simplified Results page

--- src/pages/Results/Results.js
+++ src/pages/Results/Results.js
@@ -1,37 +1,19 @@
-import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Layout from "../../components/Layouts/Layout";
 import Styles from "../../styles/pages/Results/results.module.css";
 import GC from "./GC/GC";
-import Interiit from "./InterIIT/InterIIT";
+import InterIIT from "./InterIIT/InterIIT";
 
 export default function Results() {
-  document.title = "Results | TSG";
-
-  // State variables for conditional rendering
-  const [gc, setGC] = useState(true);
-  const [interIIT, setInterIIT] = useState(false);
-
   const location = useLocation();
-  useEffect(() => {
-    if (location.pathname === "/results") {
-      setGC(true);
-      setInterIIT(false);
-    } else if (location.pathname === "/results/gc") {
-      setGC(true);
-      setInterIIT(false);
-    } else if (location.pathname === "/results/interiit") {
-      setGC(false);
-      setInterIIT(true);
-    }
-  }, [location.pathname]);
+  document.title = "Results | TSG";
 
   return (
     <Layout>
       <div className={Styles.resultsSection}>
         <div className={Styles.content}>
-          {gc && <GC />}
-          {interIIT && <Interiit />}
+          {location.pathname === "/results/gc" && <GC />}
+          {location.pathname === "/results/interiit" && <InterIIT />}
         </div>
       </div>
     </Layout>
