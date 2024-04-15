b7898aeb035e0c7eb5c35780fc2bb20c80605d86
proffapt <proffapt@pm.me>
2024-04-15 20:05:33 +0530

    fix: syncing with remote repo

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Make sure we are in the correct directory
 cd /home/techcoordi/tsg-site/frontend || exit 1
-git pull origin main
+
+# Sync with remote repository
+git fetch origin
+git reset --hard origin/main
+
+# Build Stage
 sudo docker build -t proffapt/tsg-site-frontend .
+
+# Deploy Stage
 sudo docker-compose down
 sudo docker-compose up -d
