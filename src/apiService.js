dfb11429a4f0c8da1ede4f72cb88bed36973b18a
Parth Gupta <parthgupta2510@gmail.com>
2024-07-31 16:48:32 +0530

    add get contacts api

--- src/apiService.js
+++ src/apiService.js
@@ -1,4 +1,4 @@
-// const apiService = 'http://localhost:5050';
-const apiService = 'https://gymkhana.iitkgp.ac.in';
+// const apiService = 'http://localhost:5050/api';
+const apiService = 'https://gymkhana.iitkgp.ac.in/api';
 
 export default apiService;
