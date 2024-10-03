0f5870b642639b69b4bed3d9e85679ccf5064882
Harsh Bansal <harshbansal8705@gmail.com>
2024-10-03 18:10:34 +0530

    Revert "remove this commit to make website visible"
    This reverts commit 867d5c943af9f8f0cc034474c171b5dde2ed63c4.

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -5,50 +5,35 @@ import AboutUs from "./AboutUs/AboutUs";
 import CommitteesHome from "./Committee/CommitteesHome";
 import Societies from "./Societies/Societies";
 import Greet from "./Greet";
-import { BorderWidth } from "react-bootstrap-icons";
 
 export default function Home() {
-  // document.title = "Home | TSG";
-  document.title = "502 Bad Gateway";
-  
-    return (
-        // <Layout>
-        //   <div className={Styles.backgroundImage}>
-        //     <div className={Styles.textOverlay}>
-        //       <div className={Styles.kgpSvg}>
-        //         <img
-        //           src="/data/media/images/general/gymkhanaLogo.png"
-        //           alt="KGP Logo"
-        //         />
-        //       </div>
-        //       <div className={Styles.greetMessage}>
-        //         <Greet />
-        //       </div>
-        //     </div>
-        //   </div>
-        //   <div>
-        //     {/* About Us Section */}
-        //     <AboutUs />
+  document.title = "Home | TSG";
 
-        //     {/* Committees Section */}
-        //     <CommitteesHome />
-
-        //     {/* Socities Section */}
-        //     <Societies />
-        //   </div>
-        // </Layout>
-        <div
-            style={{
-                width: "100vw",
-                height: "100vh",
-                backgroundColor: "white",
-            }}
-        >
-            <center>
-                <h1 style={{color: "black", fontFamily: "sans serif", fontWeight: "bold", paddingTop: "10px", paddingBottom: "8px", fontSize: "32px", marginBottom: "0px"}}>502 Bad Gateway</h1>
-            </center>
-            <hr style={{display: "block", borderWidth: "1px", opacity: "1"}} />
-            <center style={{fontFamily: "sans serif", fontSize: "16px"}}>nginx</center>
+  return (
+    <Layout>
+      <div className={Styles.backgroundImage}>
+        <div className={Styles.textOverlay}>
+          <div className={Styles.kgpSvg}>
+            <img
+              src="/data/media/images/general/gymkhanaLogo.png"
+              alt="KGP Logo"
+            />
+          </div>
+          <div className={Styles.greetMessage}>
+            <Greet />
+          </div>
         </div>
-    );
+      </div>
+      <div>
+        {/* About Us Section */}
+        <AboutUs />
+
+        {/* Committees Section */}
+        <CommitteesHome />
+
+        {/* Socities Section */}
+        <Societies />
+      </div>
+    </Layout>
+  );
 }
