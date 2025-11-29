f71dc89debeafab630d3494b475ff040549225bf
Daksh Yadav <dakshyadav1810@gmail.com>
2025-11-29 19:30:42 +0530

    removed static metadata and added Helmet

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Layout from "../../components/Layouts/Layout";
 import Styles from "../../styles/pages/home.module.css";
 import AboutUs from "./AboutUs/AboutUs";
@@ -7,10 +8,34 @@ import Societies from "./Societies/Societies";
 import Greet from "./Greet";
 
 export default function Home() {
-  document.title = "Home | TSG";
+  const pageTitle = "Technology Students' Gymkhana - IIT Kharagpur | TSG";
+  const pageDescription = "Technology Students' Gymkhana is the hub of the numerous extra-curricular and co-curricular activities in IIT Kharagpur ranging from sports to socio-cultural. The Gymkhana is managed by the students, for the students, under the guidance and active participation of the faculty and staff members.";
+  const baseUrl = typeof window !== "undefined" ? window.location.origin : "https://gymkhana.iitkgp.ac.in";
+  const pageImage = `${baseUrl}/data/media/images/general/gymkhanaLogo.png`;
+  const pageUrl = typeof window !== "undefined" ? window.location.href : baseUrl;
 
   return (
     <Layout>
+      <Helmet>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:title" content="Technology Students' Gymkhana - IIT Kharagpur" />
+        <meta property="og:description" content="The hub of extra-curricular and co-curricular activities in IIT Kharagpur. Managed by students, for students." />
+        <meta property="og:image" content={pageImage} />
+        <meta property="og:site_name" content="Technology Students' Gymkhana - IIT Kharagpur" />
+        
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={pageUrl} />
+        <meta name="twitter:title" content="Technology Students' Gymkhana - IIT Kharagpur" />
+        <meta name="twitter:description" content="The hub of extra-curricular and co-curricular activities in IIT Kharagpur. Managed by students, for students." />
+        <meta name="twitter:image" content={pageImage} />
+      </Helmet>
+      
       <div className={Styles.backgroundImage}>
         <div className={Styles.textOverlay}>
           <div className={Styles.kgpSvg}>
